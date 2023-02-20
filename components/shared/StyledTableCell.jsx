dd9a6ca45237c33ee517a363e6a4479ba654c686
AaradDarman <50143241+AaradDarman@users.noreply.github.com>
2023-02-20 08:31:04 +0330

    Make Table Font Responsive

--- components/shared/StyledTableCell.jsx
+++ components/shared/StyledTableCell.jsx
@@ -6,16 +6,23 @@ export const StyledTableCell = muiStyled(TableCell)(({ theme }) => ({
     backgroundColor: theme.palette.primary.main,
     borderColor: "rgba(255, 255, 255, 0.23)",
     padding: "6px",
+    fontSize: 12,
     [theme.breakpoints.up("xl")]: {
       padding: "6px 16px",
     },
+    [theme.breakpoints.up("xs")]: {
+      fontSize: 14,
+    },
   },
   [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
+    fontSize: 12,
     borderColor: "rgba(255, 255, 255, 0.23)",
     padding: "6px",
     [theme.breakpoints.up("xl")]: {
       padding: "6px 16px",
+    },
+    [theme.breakpoints.up("xs")]: {
+      fontSize: 14,
     },
   },
 }));
